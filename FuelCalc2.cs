dd853604bd919a159db3a6d51d0e1ea77afdc74c
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2024-01-22 03:14:10 +0200

    6 Review method to SetPosition of Labels and Buttons.

--- FuelCalc2.cs
+++ FuelCalc2.cs
@@ -29,16 +29,24 @@ namespace C__OOP_HW014_WinForms_FuelCalc_r00
             priceLabel.Location = new Point(consumeLabel.Location.X + consumeLabel.Width + 15, space);
 
             //set position of textbox
-            int space2 = 10;
-            distanceTB.Location = new Point(distanceLabel.Location.X + (distanceLabel.Width - distanceTB.Width)/2, distanceLabel.Location.Y + distanceTB.Height + space2);
-            consumeTB.Location = new Point(consumeLabel.Location.X + (consumeLabel.Width - consumeTB.Width)/2, consumeLabel.Location.Y + consumeTB.Height + space2);
-            priceTB.Location = new Point(priceLabel.Location.X + (priceLabel.Width - priceTB.Width)/2, priceLabel.Location.Y + priceTB.Height + space2);
+            distanceTB.Location = SetPositionTB(distanceLabel, distanceTB, 10);
+            consumeTB.Location = SetPositionTB(consumeLabel, consumeTB, 10);
+            priceTB.Location = SetPositionTB(priceLabel, priceTB, 10);
 
             //set position of button
-            int space3 = 0;
-            calcBtn.Location = new Point(distanceLabel.Location.X + (distanceLabel.Width - calcBtn.Width) / 2, distanceTB.Location.Y + calcBtn.Height + space3);
-            clearBtn.Location = new Point(consumeLabel.Location.X + (consumeLabel.Width - clearBtn.Width) / 2, consumeTB.Location.Y + clearBtn.Height + space3);
-            saveBtn.Location = new Point(priceLabel.Location.X + (priceLabel.Width - saveBtn.Width) / 2, priceTB.Location.Y + saveBtn.Height + space3);
+            calcBtn.Location = SetPositionBtn(distanceLabel, calcBtn, distanceTB, 0);
+            clearBtn.Location = SetPositionBtn(consumeLabel, clearBtn, consumeTB, 0);
+            saveBtn.Location = SetPositionBtn(priceLabel, saveBtn, priceTB, 0);
+        }
+
+        private Point SetPositionTB(in Label label, in TextBox TB, int space)
+        {
+            return new Point(label.Location.X + (label.Width - TB.Width) / 2, label.Location.Y + TB.Height + space);
+        } 
+        
+        private Point SetPositionBtn(in Label label, in Button Btn, in TextBox TB, int space)
+        {
+            return new Point(label.Location.X + (label.Width - Btn.Width) / 2, TB.Location.Y + Btn.Height + space);
         }
 
         private void setLabelFontBold(params Label[] label)
